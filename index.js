e7ffe2132bd62e0e157ba70a6a4e97c8d057c60e
Astro <danielfx016@gmail.com>
2024-09-29 08:21:57 +0100

    fix

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const fs = require("fs").promises;
 const path = require("path");
 const config = require("./config");
-const { connect, getandRequirePlugins } = require("./lib");
+const { connect, getandRequirePlugins, requireJS } = require("./lib");
 global.__basedir = __dirname;
 
-const readAndRequireFiles = async (directory) => {
-  const files = await fs.readdir(directory);
-  return (await Promise.all(
-    files
-      .filter(file => path.extname(file).toLowerCase() === '.js')
-      .map(async file => {
-        try {
-          return require(path.join(directory, file));
-        } catch (error) {
-          console.error(`Error in file ${file}:${error.lineNumber || ''} - Skipping`);
-          return null;
-        }
-      })
-  )).filter(Boolean);
-};
 
 async function initialize() {
-
-  await readAndRequireFiles(path.join(__dirname, "/lib/Store/"));
+  await requireJS(path.join(__dirname, "/lib/Store/"));
   console.log("Syncing Database");
   await config.DATABASE.sync();
   console.log("⬇  Installing Plugins...");
-  await readAndRequireFiles(path.join(__dirname, "/plugins/"));
+  await requireJS(path.join(__dirname, "/plugins/"));
   await getandRequirePlugins();
   console.log("✅ Plugins Installed!");
   return await connect();
