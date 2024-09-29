1b74115292fb112c2ffebbe658c71522ec9c6ec4
Astro <danielfx016@gmail.com>
2024-09-29 17:51:46 +0100

    fix: express

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const express = require('express');
 const path = require('path');
 const config = require('./config');
 const { connect, getandRequirePlugins, requireJS } = require('./lib');
+
+const app = express();
+const PORT = process.env.PORT || '8000';
+
 async function initialize() {
- await requireJS(path.join(__dirname, '/lib/Store/'));
- console.log('Syncing Database');
- await config.DATABASE.sync();
- console.log('⬇  Installing Plugins...');
- await requireJS(path.join(__dirname, '/plugins/'));
- await getandRequirePlugins();
- console.log('✅ Plugins Installed!');
- return await connect();
+  await requireJS(path.join(__dirname, '/lib/Store/'));
+  await config.DATABASE.sync();
+  await requireJS(path.join(__dirname, '/plugins/'));
+  await getandRequirePlugins();
+  return connect();
 }
 
-initialize();
+app.listen(PORT, async () => {
+  return await initialize();
+});
