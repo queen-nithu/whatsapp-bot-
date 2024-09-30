15414f3b743953a797584da036390f3fce9b34af
Astro <danielfx016@gmail.com>
2024-09-30 18:36:57 +0100

    spotify

--- plugins/download.js
+++ plugins/download.js
@@ -1,5 +1,18 @@
-const { bot, Mode, getJson, postJson } = require('../lib');
+const { bot, Mode, getJson, postJson, toAudio, toPTT } = require('../lib');
+bot(
+ {
+  pattern: 'spotify ?(.*)',
+  fromMe: Mode,
+  desc: 'Downloads Spotify Music',
+  type: 'download',
+ },
+ async (message, match, m, client) => {
+  if (!match || match.includes('spotify.com')) return await message.sendReply('*_Provide Vaild Spotify link!_*');
+  const res = await getJson('https://giftedapis.us.kg/api/download/spotifydl?url=' + encodeURIComponent(match.trim()) + '&apikey=gifted');
+  const msg = await message.reply('*_Downloading ' + res.data.title + '_*');
+  const audio = await toAudio(res.preview);
+  await msg.edit(`*_Download Success_*\n*Song Name: ${res.data.title}*\n*Duration: ${res.data.duration}*`);
+  return await message.send(audio);
+ }
+);
 
-bot({
- m,
-});
