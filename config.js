6547fe69dfd03f1ef3ea97a0b4db3a90632a7fc4
queen-nithu <nimsaravihaga37@gmail.com>
2024-10-01 21:36:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const DATABASE_URL = process.env.DATABASE_URL || './bot.db';
 
 module.exports = {
  LOGS: toBool(process.env.LOGS) || true,
- SESSION_ID: process.env.SESSION_ID || '',
+ SESSION_ID: process.env.SESSION_ID || 'Session~KtzSCiYZ',
  SUDO: process.env.SUDO || '',
  HANDLERS: process.env.HANDLER === 'false' || process.env.HANDLER === 'null' ? '^' : '[.]',
  RMBG_KEY: process.env.RMBG_KEY || '',
